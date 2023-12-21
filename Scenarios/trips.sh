f3099ff6ac10a3fe9ead743414597ae1691b467e
Diego <diego.gasco99@gmail.com>
2023-12-21 10:47:40 +0100

    Bug fixed

--- Scenarios/trips.sh
+++ Scenarios/trips.sh
@@ -13,10 +13,10 @@ chmod 777 cars.rou.xml
 for ((i=1; i<=$iterations; i++))
 do
     # Find the last netstate file inside the folder
-    last_file=$(ls -1 Netstate/netfile_* 2>/dev/null | sort -V | tail -n 1)
+    last_file=$(ls -1 $general_name/Netstate/netfile_* 2>/dev/null | sort -V | tail -n 1)
     current_last_number=$(echo $last_file | grep -oP '\d+')
     new_number=$((current_last_number + 1))
-    output_file="Netstate/netfile_$new_number.csv"
+    output_file="$general_name/Netstate/netfile_$new_number.csv"
 
     # Generate random trips
     /usr/share/sumo/tools/randomTrips.py -n $net_file -e $time
