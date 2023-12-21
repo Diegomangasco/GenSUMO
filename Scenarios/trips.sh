a69c08a8ebd54ff29a7dfcb95f2e39ada4d798d9
Diego <diego.gasco99@gmail.com>
2023-12-21 10:45:51 +0100

    Bug fixed

--- Scenarios/trips.sh
+++ Scenarios/trips.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 folder=$1
-time=$2
-iterations=$3
+general_name=$2
+time=$3
+iterations=$4
 cd $folder
 
-net_file="$folder.net.xml"
+net_file="$general_name.net.xml"
 touch cars.rou.xml
 chmod 777 cars.rou.xml
 
@@ -16,17 +17,17 @@ do
     current_last_number=$(echo $last_file | grep -oP '\d+')
     new_number=$((current_last_number + 1))
     output_file="Netstate/netfile_$new_number.csv"
-    chmod 777 $output_file
 
     # Generate random trips
     /usr/share/sumo/tools/randomTrips.py -n $net_file -e $time
     duarouter -t trips.trips.xml -n $net_file -o cars.rou.xml
 
     # Generate netstate  and amitran (trajectories) file
-    sumo -c $folder.sumo.cfg --netstate-dump netstate.xml --amitran-output amitran.xml
+    sumo -c $general_name.sumo.cfg --netstate-dump netstate.xml --amitran-output amitran.xml
     chmod 777 netstate.xml
     chmod 777 amitran.xml
     python tools/xml/xml2csv.py netstate.xml --output $output_file
+    chmod 777 $output_file
 
     > trips.trips.xml
     > cars.rou.xml
