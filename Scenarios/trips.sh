cd765c0c1d559f9f9566da3c12c8184f68890a54
Diego Gasco <diego.gasco99@gmail.com>
2023-12-29 00:23:44 +0100

    Added pedestrians and log file

--- Scenarios/trips.sh
+++ Scenarios/trips.sh
@@ -7,29 +7,56 @@ iterations=$4
 cd $folder/$scenario_name
 
 net_file="$scenario_name.net.xml"
-touch cars.rou.xml
-chmod 777 cars.rou.xml
+
+python3 ../search_crossing_edges.py $net_file
+
+# Only balanced situation between different ways until now
+balanced="yes"
 
 for ((i=1; i<=$iterations; i++))
 do
+    > netstate.xml
+    > cars.rou.xml
+    > cars.rou.alt.xml
+    > pedestrians.rou.xml
+    > pedestrians.rou.alt.xml
+    > trips.trips.xml
+    > logfile.txt
+
     # Find the last netstate file inside the folder
     last_file=$(ls -1 Netstate/netfile_* 2>/dev/null | sort -V | tail -n 1)
     current_last_number=$(echo $last_file | grep -oP '\d+')
     new_number=$((current_last_number + 1))
-    
-    # Generate random trips
-    /usr/share/sumo/tools/randomTrips.py -n $net_file -e $time
-    duarouter -t trips.trips.xml -n $net_file -o cars.rou.xml
+
+    # Lambda possible values
+    lambda_values=(0.1 0.2 0.3 0.4 0.5 0.6 0.7)
+    # Get the length of the options array
+    num_options=${#lambda_values[@]}
+    # Generate a random index
+    random_index=$((RANDOM % num_options))
+    # Make the random choice
+    chosen_lambda=${lambda_values[$random_index]}
+    # Generate random trips for vehicles
+    python3 random_trips.py -t $time -l $chosen_lambda -b $balanced
+
+    # Generate random trips for pedestrians
+    /usr/share/sumo/tools/randomTrips.py -n $net_file -e $time --pedestrians --random --fringe-factor 10
+    duarouter -r trips.trips.xml -n $net_file -o pedestrians.rou.xml
 
     # Generate netstate file
-    sumo -c $scenario_name.sumo.cfg --netstate-dump netstate.xml
-    chmod 777 netstate.xml
+    sumo -c $scenario_name.sumo.cfg --netstate-dump netstate.xml --collision.action warn --intermodal-collision.action warn --log logfile.txt
 
     # Generate the dataset
-    python3 ../dataset_creation.py $new_number
+    python3 ../dataset_creation.py
 
-    > trips.trips.xml
-    > cars.rou.xml
 done
+
+> netstate.xml
+> cars.rou.xml
+> cars.rou.alt.xml
+> pedestrians.rou.xml
+> pedestrians.rou.alt.xml
+> trips.trips.xml
+> logfile.txt
 
 exit 1
